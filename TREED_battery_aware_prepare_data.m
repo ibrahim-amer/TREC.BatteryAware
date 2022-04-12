b5d1857c1c2ea971158db262fb16916337f96657
Ibrahim Amer <MY_NAME@example.com>
2022-04-12 00:15:25 -0400

    comit

--- TREED_battery_aware_prepare_data.m
+++ TREED_battery_aware_prepare_data.m
@@ -1,5 +1,5 @@
 function [dataObj] = TREED_battery_aware_prepare_data(dataObj)
-    dataObj.numOfVars = dataObj.N .* dataObj.M + dataObj.M;
+    dataObj.numOfVars = dataObj.N .* dataObj.M;
     %% Problem formulation and system model can be found here: 
     % https://skillful-honesty-f66.notion.site/Meeting-Preparation-March-23-2022-b7f0da29e5694554ba0f07d0acefe679
     %% Preparing Constants: 
@@ -13,11 +13,20 @@ function [dataObj] = TREED_battery_aware_prepare_data(dataObj)
     dataObj.worker_CPU_ToVal = 4e9;
     dataObj.wrokers_freqs = dataObj.worker_CPU_FromVal + (dataObj.worker_CPU_ToVal - dataObj.worker_CPU_FromVal) * rand(1, dataObj.N);  % size  = N
     dataObj.wrokers_freqs = round(dataObj.wrokers_freqs, 2);
+    
     %Workers maximum number of tasks
     dataObj.worker_max_tasks_fromval = 1;
     dataObj.worker_max_tasks_toval = 3;
     dataObj.workers_max_tasks = dataObj.worker_max_tasks_fromval + (dataObj.worker_max_tasks_toval - dataObj.worker_max_tasks_fromval) * rand(1, dataObj.N);  % size  = N
     dataObj.workers_max_tasks = round(dataObj.workers_max_tasks);
+    
+    %Workers battery capacities
+    %Workers maximum number of tasks
+    dataObj.worker_battery_fromval = 0;
+    dataObj.worker_battery_toval = 1;
+    dataObj.workers_battery_caps = dataObj.worker_battery_fromval + (dataObj.worker_battery_toval - dataObj.worker_battery_fromval) * rand(1, dataObj.N);  % size  = N
+    %dataObj.workers_battery_caps = round(dataObj.workers_max_tasks);
+    dataObj.workers_max_batt_cap = max(dataObj.workers_battery_cap);
     %Kappa 
     dataObj.kappa = 1e-29;
     
@@ -32,6 +41,13 @@ function [dataObj] = TREED_battery_aware_prepare_data(dataObj)
     dataObj.task_dataSize_toVal = 50e6;
     dataObj.tasks_dataSize = dataObj.task_dataSize_fromVal + (dataObj.task_dataSize_toVal - dataObj.task_dataSize_fromVal) * rand(1, dataObj.M);  % size  = M
     dataObj.tasks_dataSize = round(dataObj.tasks_dataSize, 2);
+    
+    %Tasks CPU requirement
+    dataObj.task_CPU_fromVal = 1;
+    dataObj.task_CPU_toVal = 2;
+    dataObj.tasks_CPU_req = dataObj.task_CPU_fromVal + (dataObj.task_CPU_toVal - dataObj.task_CPU_fromVal) * rand(1, dataObj.M);  % size  = M
+    dataObj.tasks_CPU_req = round(dataObj.tasks_dataSize, 2);
+    
     %Computation delays
     tasks_specs = dataObj.tasks_pdensity .* dataObj.tasks_dataSize; % vectorSize = M
     dataObj.tasks_comp_delays = [];
@@ -51,25 +67,79 @@ function [dataObj] = TREED_battery_aware_prepare_data(dataObj)
     dataObj.A = [];
     dataObj.b = [];
     dataObj.operators = [];
-    %Constraint (3b)
-    con_3b = zeros(dataObj.N * dataObj.M, dataObj.numOfVars);
-    for i = 1:dataObj.N * dataObj.M
-        row = zeros(1, dataObj.N .* dataObj.M + dataObj.M);
-        row(i) = 1 .* dataObj.tasks_comp_delays(i);
-        con_3b(i, :) = row;
-    end
-    con_3b_bounds = zeros(dataObj.N * dataObj.M, 1);
+    %% Constraint w^{\text{CPU}}_i\lambda_{ij} >= t^{\text{CPU}}_j (b)
+    con_b = zeros(dataObj.N * dataObj.M, dataObj.numOfVars);
+    con_b_bounds = zeros(dataObj.N * dataObj.M, 1);
     ctr = 1;
     for i = 1:dataObj.N
         for j = 1:dataObj.M
-            con_3b_bounds(ctr, :) = dataObj.tasks_deadlines(j);
+            row = zeros(1, dataObj.numOfVars);
+            row(i + j - 1) = 1 .* dataObj.wrokers_freqs(i);
+            con_b(ctr, :) = row;
+            con_b_bounds(ctr, :) = dataObj.tasks_CPU_req(j);
+        end
+    end
+    dataObj.A = [dataObj.A; con_b];
+    dataObj.b = [dataObj.b; con_b_bounds];
+    dataObj.operators = [dataObj.operators repmat('>', 1, size(con_b, 1))];
+    dataObj.con_b_size = size(con_b, 1);
+    
+    
+    %% Constraint \sum^N_{i =1} w^{\text{cap}}_i\lambda_{ij} >= w^{\text{cap}}_{\max} (c)
+    con_c = zeros(dataObj.M, dataObj.numOfVars);
+    con_c_bounds = zeros(dataObj.M, 1);
+    ctr = 1;
+    for j = 1:dataObj.M
+        row = zeros(1, dataObj.numOfVars);
+        for i = j:dataObj.M:dataObj.N * dataObj.M
+            row(i) = dataObj.workers_battery_caps(i / dataObj.max_batt_cap);
+            con_c_bounds(ctr) = dataObj.workers_max_batt_cap;
+        end
+        con_c(j) = row;
+    end
+    dataObj.A = [dataObj.A; con_c];
+    dataObj.b = [dataObj.b; con_c_bounds];
+    dataObj.operators = [dataObj.operators repmat('>', 1, size(con_c, 1))];
+    dataObj.con_c_size = size(con_c, 1);
+    
+    %% Constraint \tau^{\text{comp}}_{ij}\lambda_{ij} <=  t^{\text{deadline}}_j (d)
+    con_d = zeros(dataObj.N * dataObj.M, dataObj.numOfVars);
+    for i = 1:dataObj.N * dataObj.M
+        row = zeros(1, dataObj.N .* dataObj.M + dataObj.M);
+        row(i) = 1 .* dataObj.tasks_comp_delays(i);
+        con_d(i, :) = row;
+    end
+    con_d_bounds = zeros(dataObj.N * dataObj.M, 1);
+    ctr = 1;
+    for i = 1:dataObj.N
+        for j = 1:dataObj.M
+            con_d_bounds(ctr, :) = dataObj.tasks_deadlines(j);
             ctr = ctr + 1;
         end
     end
-    dataObj.A = [dataObj.A; con_3b];
-    dataObj.b = [dataObj.b; con_3b_bounds];
-    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_3b, 1))];
-    % Constraint (3c) 
+    dataObj.A = [dataObj.A; con_d];
+    dataObj.b = [dataObj.b; con_d_bounds];
+    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_d, 1))];
+    dataObj.con_d_size = size(con_d, 1);
+    
+    %% Constraint \sum^{M}_{j=1} \lambda_{ij} <= w^{\text{tasks}}_i(e)
+    con_e = zeros(dataObj.N, dataObj.numOfVars);
+    ctr = 1;
+    for i = 0:dataObj.M:dataObj.N * dataObj.M - 1
+        row = zeros(1, dataObj.numOfVars);
+        for j = 1:dataObj.M
+            row(1, i+j) = 1;            
+        end
+        con_e(ctr, :) = row;
+        ctr = ctr + 1;
+    end
+    
+    dataObj.A = [dataObj.A; con_e];
+    dataObj.b = [dataObj.b; dataObj.workers_max_tasks'];
+    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_e, 1))];
+    dataObj.con_e_size = size(con_e, 1);
+    
+    %% Constraint (ee) 
     dataObj.max_energy = 3;%was 1
     dataObj.max_energies = dataObj.max_energy .* ones(1, dataObj.N);%size N
     energy_task_specs_tmp = dataObj.kappa .* dataObj.tasks_dataSize .* dataObj.tasks_pdensity; %size M
@@ -81,96 +151,42 @@ function [dataObj] = TREED_battery_aware_prepare_data(dataObj)
             ctr = ctr + 1;
         end
     end
-%     con_3b = zeros(dataObj.N * dataObj.M, dataObj.numOfVars);
-%     for i = 1:dataObj.N * dataObj.M
-%         row = zeros(1, dataObj.numOfVars);
-%         row(i) = dataObj.comp_energies(i);
-%         con_3b(i, :) = row;
-%     end
-    con_3c = zeros(dataObj.N * dataObj.M, dataObj.numOfVars);
+    con_ee = zeros(dataObj.N * dataObj.M, dataObj.numOfVars);
     ctr = 1;
     for i = 0:dataObj.M:dataObj.N * dataObj.M - 1
         row = zeros(1, dataObj.numOfVars);
         for j = 1:dataObj.M
             row(1, i+j) = 1;            
         end
-        con_3c(ctr, :) = row;
+        con_ee(ctr, :) = row;
         ctr = ctr + 1;
     end
     
-    con_3c_bounds = zeros(dataObj.N * dataObj.M, 1);
+    con_ee_bounds = zeros(dataObj.N * dataObj.M, 1);
     ctr = 1;
     for i = 1:dataObj.N
         for j = 1:dataObj.M
-            con_3c_bounds(ctr, :) = dataObj.max_energies(i);
+            con_ee_bounds(ctr, :) = dataObj.max_energies(i);
             ctr = ctr + 1;
         end
     end
     
-    dataObj.A = [dataObj.A; con_3c];
-    dataObj.b = [dataObj.b;  con_3c_bounds];
-    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_3c, 1))];
-    % Constraint (3d) (N)
-    con_3d = zeros(dataObj.N, dataObj.numOfVars);
-    ctr = 1;
-    for i = 0:dataObj.M:dataObj.N * dataObj.M - 1
-        row = zeros(1, dataObj.numOfVars);
-        for j = 1:dataObj.M
-            row(1, i+j) = 1;            
-        end
-        con_3d(ctr, :) = row;
-        ctr = ctr + 1;
-    end
+    dataObj.A = [dataObj.A; con_ee];
+    dataObj.b = [dataObj.b;  con_ee_bounds];
+    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_ee, 1))];
+    dataObj.con_e_size = size(con_ee, 1);
+
+    %% Constraint (f) RHS
     
-    dataObj.A = [dataObj.A; con_3d];
-    dataObj.b = [dataObj.b; dataObj.workers_max_tasks'];
-    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_3d, 1))];
-    %Constraint (3e)
-    con_3e = zeros(dataObj.M, dataObj.numOfVars);
-    dataObj.Aeq = [];
-    dataObj.beq = [];
-    for i = 1:dataObj.M
-        row = zeros(1, dataObj.numOfVars);
-        count = 0;
-        for j = 1:dataObj.N
-            row(1, i+count) = 1;            
-            count = count + dataObj.M;
-        end
-        max_delay = zeros(1, dataObj.N .* dataObj.M + dataObj.M);
-        max_delay(1, dataObj.N .* dataObj.M + i) = -1;
-        row = row + max_delay;
-        con_3e(i, :) = row;
-    end
-    dataObj.Aeq = [dataObj.Aeq; con_3e];
-    dataObj.beq = [dataObj.beq; zeros(dataObj.M, 1);];
-    %merge inequalities with equalities 
-    dataObj.A = [dataObj.A; dataObj.Aeq];
-    dataObj.b = [dataObj.b; dataObj.beq];
-    dataObj.operators = [dataObj.operators repmat('=', 1, size(dataObj.Aeq, 1))];
-    %Constraint (3f) RHS
-    con_3f_rhs = zeros(1, dataObj.N .* dataObj.M + dataObj.M);
-    con_3f_rhs(1, (dataObj.N * dataObj.M + 1) : end) = 1;
-    dataObj.A = [dataObj.A; con_3f_rhs];
-    dataObj.b = [dataObj.b; sum(dataObj.workers_max_tasks)];
-    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_3f_rhs, 1))];
-    %Constraint (3f) LHS
-    con_3f_lhs = zeros(1, dataObj.N .* dataObj.M + dataObj.M);
-    con_3f_lhs(1, (dataObj.N * dataObj.M + 1) : end) = -1;
-    dataObj.A = [dataObj.A; con_3f_lhs];
-    dataObj.b = [dataObj.b; -1 .* (dataObj.M ./ dataObj.N)];
-    dataObj.operators = [dataObj.operators repmat('<', 1, size(con_3f_lhs, 1))];
-    
-    dataObj.ub = zeros(1, dataObj.N * dataObj.M + dataObj.M);
-    dataObj.lb = zeros(1, dataObj.N * dataObj.M + dataObj.M);
+    dataObj.ub = zeros(1, dataObj.N * dataObj.M);
+    dataObj.lb = zeros(1, dataObj.N * dataObj.M);
     dataObj.ub(1, 1:dataObj.N * dataObj.M) = 1;
     dataObj.lb(1, 1:dataObj.N * dataObj.M) = 0; %already satisfied
-    dataObj.ub(1, dataObj.N * dataObj.M + 1:end) = dataObj.N;
-    dataObj.lb(1, dataObj.N * dataObj.M + 1: end) = 1;
     
     
     %Upper and lower bounds
     %lambdas upper bounds
-    lambdasBounds = diag([ones(dataObj.N .* dataObj.M, 1); zeros(dataObj.M, 1)]);
+    lambdasBounds = diag([ones(dataObj.N .* dataObj.M, 1);]);
     lambdasBounds = lambdasBounds(1:dataObj.N * dataObj.M, :);
     dataObj.A = [dataObj.A; lambdasBounds];
     dataObj.b = [dataObj.b; dataObj.ub(1, 1:dataObj.N * dataObj.M)'];
@@ -179,16 +195,7 @@ function [dataObj] = TREED_battery_aware_prepare_data(dataObj)
     dataObj.A = [dataObj.A; lambdasBounds];
     dataObj.b = [dataObj.b; dataObj.lb(1, 1:dataObj.N * dataObj.M)'];
     dataObj.operators = [dataObj.operators repmat('>', 1, dataObj.N * dataObj.M)];
-    %replicas upper bounds
-    replicasBounds = diag([zeros(dataObj.N .* dataObj.M, 1); ones(dataObj.M, 1)]);
-    replicasBounds = replicasBounds(dataObj.N * dataObj.M + 1:end, :);
-    dataObj.A = [dataObj.A; replicasBounds];
-    dataObj.b = [dataObj.b; dataObj.ub(1, dataObj.N * dataObj.M + 1:end)'];
-    dataObj.operators = [dataObj.operators repmat('<', 1, dataObj.M)];
-    %replicas lower bounds
-    dataObj.A = [dataObj.A; replicasBounds];
-    dataObj.b = [dataObj.b; dataObj.lb(1, dataObj.N * dataObj.M + 1:end)'];
-    dataObj.operators = [dataObj.operators repmat('>', 1, dataObj.M)];
+    dataObj.bounds_size = size(2 * lambdasBounds, 1);
     
     %%
 end
--- TREED_battery_aware_slice_data.m
+++ TREED_battery_aware_slice_data.m
@@ -1,7 +1,7 @@
 function [dataObj] = TREED_battery_aware_slice_data(N, M, dataObj)
     dataObj.N = N;
     dataObj.M = M;
-    dataObj.numOfVars = dataObj.N * dataObj.M + dataObj.M;
+    dataObj.numOfVars = dataObj.N * dataObj.M;
     dataObj.wrokers_freqs = dataObj.wrokers_freqs(:, 1:N);
     dataObj.workers_max_tasks = dataObj.workers_max_tasks(:, 1:N);
     dataObj.tasks_pdensity = dataObj.tasks_pdensity(:, 1:M);
@@ -11,14 +11,14 @@ function [dataObj] = TREED_battery_aware_slice_data(N, M, dataObj)
     dataObj.max_energies = dataObj.max_energies(:, 1:N);
     dataObj.comp_energies = dataObj.comp_energies(1:N*M, :);
     %            a     b    c   d   e   f_ub  f_lb  g_ub   g_lb
-    dataObj.con_3b_size = N*M;
-    dataObj.con_3c_size = N*M;
-    dataObj.con_3d_size = N;
-    dataObj.con_3e_size = M;
-    dataObj.con_3f_size = 2;
-    dataObj.con_3g_size = (N*M) + (N*M);
-    dataObj.con_3h_size = M + M;
-    A_b_n_rows= dataObj.con_3b_size + dataObj.con_3c_size + dataObj.con_3d_size + dataObj.con_3e_size + dataObj.con_3f_size + dataObj.con_3g_size + dataObj.con_3h_size;
+    dataObj.con_b_size = N*M;
+    dataObj.con_c_size = M;
+    dataObj.con_d_size = N*M;
+    dataObj.con_e_size = N;
+    dataObj.con_ee_size = N*M;
+    dataObj.con_bounds_size = 2 * (N*M);
+    
+    A_b_n_rows = dataObj.con_b_size + dataObj.con_c_size + dataObj.con_d_size + dataObj.con_e_size + dataObj.con_ee_size + dataObj.con_bounds_size;
     dataObj.A = dataObj.A(1:A_b_n_rows, :);
     dataObj.b = dataObj.b(1:A_b_n_rows, :);
     dataObj.Aeq = dataObj.Aeq(1:M, :);
